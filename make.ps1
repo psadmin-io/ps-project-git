24c8253bf3243304f2dbeb7c8020c476b8894130
Dan Iverson <dan.iverson@minneapolismn.gov>
2017-06-02 08:12:00 -0500

    Test if .git/hooks is already a symlink folder

--- make.ps1
+++ make.ps1
@@ -1,13 +1,17 @@
 # Requires Powershell 5 for native symlinks
 Write-Output "Making symlinks to hooks..."
-Remove-Item -recurse -force ../.git/hooks/
 
 $link = "../.git/hooks"
 $target = "../.gitpsoft/hooks"
 
+if (!(Get-ChildItem ..\.git | Where-Object { $_.LinkType -eq "SymbolicLink" })) {
+        Write-Output "`tRemoving .git/hooks directory"
+        Remove-Item $link -recurse -force         
+    }
+
 if ($PSVersionTable.PSVersion.Major -ge 5)
 {
-    New-Item -Path $link -ItemType SymbolicLink -Value $target
+    New-Item -Path $link -ItemType SymbolicLink -Value $target -Force
 }
 else
 {
